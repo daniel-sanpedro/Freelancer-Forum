02876ce4b83b060601c709d53c07ce5d2d89ce24
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-13 19:51:56 -0700

    <name changed and added html, css, js>

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Freelancer-Forum
+
+## Description
+
+Creating Freelance Forum using HTML, CSS and Javascript
+
+## Technology
+
+-HTML
+-CSS
+-JavaScript
+
+## Author
+
+Daniel San Pedro
---
+++ index.js
@@ -0,0 +1,18 @@
+// A visitor enters the website and finds a list of available freelancers. Each freelancer has a name, an occupation, and a starting price for their services. They observe on the list Alice, the writer with a starting price of $30, and Bob, who is a teacher, has a starting price of $50.
+
+// The visitor also finds a message that displays the average starting price of all the freelancers. In this example, the average starting price is $40.
+
+// A few seconds later, a new listing appears for a freelancer named Carol, who is a programmer and has a starting price of $70. The average starting price is updated to $50.
+
+// New freelancers continue to appear every few seconds, and the average starting price is updated accordingly.
+
+const freelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
