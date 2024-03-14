05f7c49ce05259cd9b4a34d769cd72ddfd3c591c
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-13 20:11:15 -0700

    <added comments>

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
 // New freelancers continue to appear every few seconds, and the average starting price is updated accordingly.
 
+
 const freelancers = [
   { name: "Dr. Slice", price: 25, occupation: "gardener" },
   { name: "Dr. Pressure", price: 51, occupation: "programmer" },
@@ -16,3 +17,21 @@ const freelancers = [
   { name: "Dr. Wire", price: 47, occupation: "teacher" },
   { name: "Prof. Goose", price: 72, occupation: "driver" },
 ];
+
+
+// every few seconds a new freelancers will be added to the available freelancers
+//        add set interval to 3000 few seconds
+
+// define arrays for possible names and occupations  
+
+// define initial array of freelancers
+
+// determine how to display freelancers information
+
+// write a function to render the initial freelancers data
+
+// write a function to generate a random freelancer
+//         is this function being called in an interval
+
+// write a function to calculate the average starting price of the freelancers array
+//         when should this function be called to update the displayed message
